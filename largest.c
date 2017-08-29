a50c55447fb102e4671a1f52ccc1efd90f7739ec
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2017-08-29 15:44:36 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+long a,b,c;
+printf("enter the number");
+scanf("%f,%f,%f",&a,&b,&c);
+if(a>b)&&(a>c)
+{
+printf("\n it is largest);
+}
+else if(b>c)
+{
+printf("\n it is largest);
+}
+else
+{
+printf("\n it is largest);
+}
+getch();
+}
