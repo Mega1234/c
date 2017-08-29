ba034f114956f7c1402d52e65f5ce3e44cdcc5c6
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2017-08-29 15:03:41 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("enter the variable");
+scanf("%d",&ch);
+if(ch=='abcdefghijklmnopqrstuvwxyz');
+{
+printf("\n it is alphabet);
+}
+else
+{
+printf("\n it is not a albhabet);
+}
+getch();
+}
