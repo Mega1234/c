d11aa4bf14a403afa94db5047cd1ebd554cf4322
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2017-08-29 15:57:55 +0530

    lklk
    gfgfdh

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,i;
+printf("\n enter the num");
+scanf("%d",&a);
+a=5;
+for(i=5;i<5;i++)
+{
+printf("\n the numbers are ");
+}
+getch();
+}
