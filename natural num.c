c1246dde6e4bc8999c808e90c1c25b8a6d9bee9a
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-01-08 16:43:52 +0530

    Create natural num.c

---
+++ natural num.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int m, i, sum = 0;
+    
+    printf("Enter a positive integer: ");
+    scanf("%d",&m);
+
+    for(i=1; i <= n; ++i)
+    {
+        sum += i;   // sum = sum+i;
+    }
+
+    printf("Sum = %d",sum);
+
+    return 0;
+}
