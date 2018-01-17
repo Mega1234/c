e9fb02310fb1c4cfd717512a75d308d460a171b2
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-01-17 16:00:45 +0530

    Create even2int.c

---
+++ even2int.c
@@ -0,0 +1,17 @@
+#inclue<stdio.h>
+#include<conio.h>
+void main();
+{
+int s1,s2,i;
+printf("enter the value");
+scanf("%d%d",&s1,&s2);
+for(i<s1;i<s2;i++)
+{
+rem=i%2;
+}
+if(i==0)
+{
+printf("%d",&i);
+}
+getch();
+}
