bfd65ab74eacaca12c80b276a76f2db20c1e5dc6
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-01-24 16:41:16 +0530

    Update countchar.c

--- countchar.c
+++ countchar.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 main()
 {
-char string[50],v;
+char s[50],v;
 int c=0,i=0;
 printf("enter the string \n");
 printf("count the number of character:\n");
 scanf("%c",&v);
-while(string [i]='\0')
+while(s [i]='\0')
 {
-if (string[i]==v)
+if (s[i]==v)
 c++;
 i++;
 }
