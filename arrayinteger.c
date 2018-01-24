607edf86d1a281e7b22fb23847a7d39239b7652b
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-01-24 16:07:06 +0530

    Create arrayinteger.c

---
+++ arrayinteger.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[20],b,i;
+float sum=0,meam=0;
+printf("enter the number of terms:");
+scanf("%d",&b);
+printf("enter the number of values:");
+scanf("%d",&a);
+for(i=0;i<b;i++);
+{
+sum=sum+a[i];
+mean=sum/b;
+}
+printf("%d",&a);
+getch();
+}
