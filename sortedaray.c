c8ae123f9bbdb78ff44f369fabdea4515fcbc8b1
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-01-24 16:32:35 +0530

    Create sortedaray.c

---
+++ sortedaray.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+ 
+int main()
+{
+int i,n,t,a[i],j.a[j];
+ 
+printf("enter the values");
+scanf("%d",&n);
+printf(("enter the values");
+for(i=0;i<n;i++)
+{
+scanf(("%d",&a[i]);
+}
+for(j=i+n;j<n,j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}}
+printf("sorted element")
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]));
+return 0;
+}}
