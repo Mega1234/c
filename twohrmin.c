cbc4ca3fd9339ca3f84b7a2bbb5732bb6a58f145
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-01-24 15:54:14 +0530

    Update and rename hrmin.c to twohrmin.c

---
+++ twohrmin.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int h1,h2,m1,m2,h,m,abs;
+printf("enter the number of hours mins:");
+scanf("%d%d",&h1,&m1);
+printf("enter the number of hours mins:");
+scanf("%d%d",&hr2,&min2);
+h=abs(h1-h2);
+m=abs(m1-m2);
+printf("%d%d",h,m);
+getch();
+}
