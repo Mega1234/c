8568d83ba9d67bab1371eaa7917afca567ac0798
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-01-31 16:21:44 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a1,b1,t1;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+printf("\nbefore swapping\n");
+printf("\na=%d and b=%d",a1,b1);
+printf("\n after swaping");
+t1=a1;
+a1=b1;
+b1=t1;
+printf("\na=%d and b=%d",a1,b1);
+return 0;
+}
