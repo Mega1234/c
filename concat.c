b64c397fd3824466babce13bd49e9124ce3e62e1
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-02-02 16:34:44 +0530

    Create concat.c

---
+++ concat.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char s1[20],s2[20];
+	
+	printf("enter the string:");
+	scanf("%s",&s1);
+	printf("enter the string 2:");
+	scanf("%s",&s2);
+	printf("the concat string is:",s1,s2);
+	return 0
+}
+	
+	
