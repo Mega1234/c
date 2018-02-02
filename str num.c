65dc59d9ee7bdeaa7e7910aaa3ee4a8f9338dbbe
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-02-02 16:08:13 +0530

    Create str num.c

---
+++ str num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int i,k;
+	char s;
+	printf("enter the string");
+	scanf("%s",&s);
+	printf("enter the number");
+	scanf("%d",&k);
+	for(i=0;i<=k;i++)
+	printf("%s",&s);
+	return 0;
+}
