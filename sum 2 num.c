c1cf80bbf8ef7ede8d4f6d22f4b67ba8595b3103
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-02-05 16:08:26 +0530

    Create sum 2 num.c

---
+++ sum 2 num.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the number:",a);
+	scanf("%d",&a);
+	printf("/n enter the number:",b);
+	scanf("%d",&b);
+	printf("/n the sum of the two number are:%d",a+b);
+	return 0;
+}
