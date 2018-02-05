b1864202f6f9c4e04edbfe975c3e4a8bd52831b8
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-02-05 16:20:33 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,n;
+	printf("enter the number:");
+	scanf("%d",&a);
+	n=2^a;
+            if(n==0)
+            {
+            	printf("yes");
+            }
+            else
+            {
+            	printf("no");
+     
+            }
+return 0;
+}
