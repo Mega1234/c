0e91ea1ada4c72dbde4e6713ba066bb50ff4bbdc
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-02-07 16:30:06 +0530

    Update alpha num.c

--- alpha num.c
+++ alpha num.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-char a[100];
+char a[10];
 int i;
 scanf("%s",a);
 for(i=0;a[i]!='\0';i++)
@@ -9,7 +9,11 @@ for(i=0;a[i]!='\0';i++)
   if((a[i]>='a'||a[i]<='z'||a[i]>='A'||a[i]<='Z')&&(a[i]>=0||a[i]<=9))
   {
     printf("yes");
-    break;
+   
+  }
+  else
+  {
+    printf("no");
   }
 }
 }
