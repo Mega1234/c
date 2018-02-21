ed1fdaaa5e1b40842bed6284fe0d7df9abfb0bc4
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-02-21 16:41:29 +0530

    Update 80.c

--- 80.c
+++ 80.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int main(void) 
+int main()
 {
-	int n,rem,rev=0;
+	int n,r,rev=0;
 	printf("enter the number:");
 	scanf("%d",&n);
 	while(n!=0)
 	{
 		rem=n%10;
-		rev=(rev*10)+rem;
+		r=(r*10)+rem;
 		n=n/10;
 	}
-	n=rev;
+	n=r;
 	while(n!=0)
 	{
 		rem=n%10;
