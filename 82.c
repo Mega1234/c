482fa99f1b1beedb1ff8cedcd7f9ab919e6dfa75
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-02-23 16:57:01 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main(void) {
+float a;
+int l,b;
+printf("length:");
+scanf("%d",&l);
+printf("\n breadth:");
+scanf("%d",&b);
+a=l*b;
+printf("\n %0.5f",a);
+	return 0;
+}
