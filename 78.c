15cad2c240fcca7343607a1ae36f880bdcbb9433
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-02-23 15:52:42 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,flag=0;
+	printf("enter the number:");
+	scanf("%d",&a);
+	if(a%13==0)
+	{
+	
+	
+	
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
