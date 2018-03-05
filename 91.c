e6b1b6f913dc5a0eea078b6863afdece4bfd411c
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-03-05 16:18:16 +0530

    Create 91.c

---
+++ 91.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("enter the three values:");
+	scanf("%d%d%d",&a,&b,&c);
+	d=a*b*c;
+	printf("\n the surface and cuboid is:%d",d);
+	return 0;
+	
+}
