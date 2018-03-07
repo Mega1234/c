53189c00637eeeae855edda1d9c972ad0c64ddea
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-03-07 16:09:02 +0530

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("\nenter the three value:");
+	scanf("%d%d%d",&a,&b,&c);
+            d=(a*b*c)/100;
+            printf("\n the floor values is..%d",d);
+	return 0;
+	
+}
