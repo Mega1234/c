81e82c37b569b3550e205ccd4baafe8673120a0a
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-03-14 16:44:42 +0530

    Create 114.c

---
+++ 114.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,i,arr[20],sum=0;
+	printf("\nenter the two values of the number:");
+	scanf("%d%d",&a,&b);
+	for(i=0;i<a;i++)
+	{
+		scanf("%d",arr[i]);
+		
+	}
+	for(i=0;i<a;i++)
+	{
+	            sum=sum+1;	
+	}
+	printf("\nthe result is:%d",sum);
+	return 0;
+}
