d85a73cf7d163a9d082a59e316960738f0b11338
Mega1234 <30749129+Mega1234@users.noreply.github.com>
2018-03-23 07:11:34 -0400

    Create pl 15.c

---
+++ pl 15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+ 
+int main() 
+{
+	char a[50],charact;
+	int i,j,count=1,max=1;
+	scanf("%[^\n]s",a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		for(j=i+1;a[j]!='\0';j++)
+		if(a[i]==a[j])
+		{
+			count=count+1;
+			if(count>max)
+			{
+				max=count;
+				charact=a[i];
+			}
+		}
+ 
+	}
+	printf("\n%c",charact);
+	return 0;
+}
